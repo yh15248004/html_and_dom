04a9ca850c22e76ae0825c8864cab0c0447de763
yh <15248004@qq.com>
2014-12-17 11:30:32 +0800

    format the code

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 function printScore() {
-  var ee = document.getElementById("score");
 
-  ee.value = getScore();
+  var scoreText = document.getElementById("score");
+  scoreText.value = getScore();
+
+  return false;
 }
 
 function getScore() {
@@ -11,18 +13,18 @@ function getScore() {
   score += getTextScore('pro010202','继承性',5);
   score += getTextScore('pro010203','多态性',5);
 
-  score += getRedioScore('radio01','B',10);
-  score += getRedioScore('radio02','A',10);
+  score += getRadioScore('radio01','B',10);
+  score += getRadioScore('radio02','A',10);
 
   score += getCheckboxScore('checkbox01','ABD',10);
   score += getCheckboxScore('checkbox02','ABC',10);
 
-  score += getRedioScore('radio04','X',10);
-  score += getRedioScore('radio05','V',10);
+  score += getRadioScore('radio04','X',10);
+  score += getRadioScore('radio05','V',10);
 
   score += getTextScore('pro0501','模型是对现实世界的简化和抽象,'+
-  '模型是对所研究的系统、过程、事物或概念的一种表达形'+
-  '式。可以是物理实体;可以是某种图形;或者是一种数学表达式。',20);
+    '模型是对所研究的系统、过程、事物或概念的一种表达形'+
+    '式。可以是物理实体;可以是某种图形;或者是一种数学表达式。',20);
 
   return score;
 }
@@ -35,7 +37,7 @@ function getTextScore(id, answer, score) {
   return count;
 }
 
-function getRedioScore(name, answer, score) {
+function getRadioScore(name, answer, score) {
   count = 0;
   var problems = document.getElementsByName(name);
   for (var i = 0; i < problems.length; i++) {
