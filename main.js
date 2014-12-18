86118483250b184c0a8531aca6d048cec8b5ff51
yh <15248004@qq.com>
2014-12-18 11:06:25 +0800

    add standard.js

--- main.js
+++ main.js
@@ -38,14 +38,14 @@ function getScore() {
   score += getTextsScore(['pro0101'],['统一建模语言'],5);
   score += getTextsScore(['pro010201','pro010202','pro010203'], ['封装性','继承性','多态性'], 15);
 
-  score += getRadioScore('radio01','B',10);
-  score += getRadioScore('radio02','A',10);
+  score += getRadioScore('radio01',['B'],10);
+  score += getRadioScore('radio02',['A'],10);
 
-  score += getCheckboxScore('checkbox01','ABD',10);
-  score += getCheckboxScore('checkbox02','ABC',10);
+  score += getCheckboxScore('checkbox01',['ABD'],10);
+  score += getCheckboxScore('checkbox02',['ABC'],10);
 
-  score += getRadioScore('radio04','X',10);
-  score += getRadioScore('radio05','V',10);
+  score += getRadioScore('radio04',['X'],10);
+  score += getRadioScore('radio05',['V'],10);
 
   score += getTextsScore(['pro0501'],['模型是对现实世界的简化和抽象,'+
     '模型是对所研究的系统、过程、事物或概念的一种表达形'+
@@ -56,14 +56,14 @@ function getScore() {
 
 function getTextsScore(ids, answers, score) {
   var result = 0;
-  var questions = [];
+  var values = [];
 
   _.forEach(ids, function(id) {
-    questions.push(document.getElementById(id));
+    values.push(document.getElementById(id).value);
   });
 
   _.forEach(answers, function(answer) {
-    var isExist = _.contains(_.map(questions, 'value'), answer);
+    var isExist = _.contains(values, answer);
     if (isExist) {
       result += score / answers.length;
     }
@@ -72,23 +72,23 @@ function getTextsScore(ids, answers, score) {
   return result;
 }
 
-function getRadioScore(name, answer, score) {
+function getRadioScore(name, answers, score) {
 
   var questions = document.getElementsByName(name);
   var question = _.find(questions, function(question) {
-    return question.checked && question.value === answer;
+    return question.checked && question.value === answers[0];
   });
   var isRight = !!question;
 
   return isRight ? score : 0;
 }
 
-function getCheckboxScore(name, answer, score) {
+function getCheckboxScore(name, answers, score) {
 
   var result = 0;
   var questions = document.getElementsByName(name);
   var selects = _.filter(questions, {checked : true});
-  if (_.map(selects, 'value').join('') === answer) {
+  if (_.map(selects, 'value').join('') === answers[0]) {
     result =  score;
   }
 
